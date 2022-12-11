1f7734e95d5520f0cb910c9faeceac7b6322583c
Adi Eyal <adi@clearforest.io>
2022-12-11 10:34:53 +0200

    Upgrading typer dependency
    See #106

--- install.py
+++ install.py
@@ -5,5 +5,6 @@ if not launch.is_installed("jinja2"):
 if not launch.is_installed("requests"):
     launch.run_pip("install requests==2.28.1", desc='requests==2.28.1')
 if not launch.is_installed("spacy"):
+    launch.run_pip("install typer==0.7.0", desc='typer==0.7.0')
     launch.run_pip("install spacy==3.0.8", desc='spacy==3.0.8')
     launch.run_pip("install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.1/en_core_web_sm-3.4.1.tar.gz", desc='Installing en_core_web_sm==3.4.1')
